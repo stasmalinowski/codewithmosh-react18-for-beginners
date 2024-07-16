74236333f69c03eb619e1be2ad8030f735a22d33
stasmalinowski <stasmalinowski@proton.me>
2024-07-16 10:53:22 +0200

    Play with using async await in useEffect hook

--- follow_along/src/App.tsx
+++ follow_along/src/App.tsx
@@ -1,32 +1,43 @@
 import { useEffect, useState } from "react";
 import { ProductList } from "./components/ProductList";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
-interface User{
-  id: number,
-  name: string
+interface User {
+  id: number;
+  name: string;
 }
 
 const App = () => {
-
-  const [users, setUsers] = useState<User[]>()
-  const [error, setError] = useState<string>("")
+  const [users, setUsers] = useState<User[]>();
+  const [error, setError] = useState<string>("");
 
   useEffect(() => {
-    axios.get<User[]>("https://jsonplxxxaceholder.typicode.com/users")
-      .then( res => setUsers(res.data))
-      .catch( err => setError(err.message) )
-  }, [])
-  
+    (async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(res.data);
+        setError("");
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    })();
+  }, []);
+
   return (
-  <>
-    <p className="text-danger">{error}</p>
-    <ul>
-      {users?.map( u => <li key={u.id}>Id: {u.id}, Name: {u.name}</li> )}
-    </ul>
-  </>
-    );
-  
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {!error &&
+          users?.map((u) => (
+            <li key={u.id}>
+              Id: {u.id}, Name: {u.name}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
 
 export default App;
