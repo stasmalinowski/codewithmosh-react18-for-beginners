ff9b25c62c321bba2d99940adb2cbcba5524d24d
stasmalinowski <stasmalinowski@proton.me>
2024-07-16 10:39:37 +0200

    Play with fetching data

--- follow_along/src/App.tsx
+++ follow_along/src/App.tsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
 import { ProductList } from "./components/ProductList";
+import axios from "axios";
+
+interface User{
+  id: number,
+  name: string
+}
 
 const App = () => {
 
-  const connect = () => console.log("Connecting...")
-  const disconnect = () => console.log("Disconnecting...")
+  const [users, setUsers] = useState<User[]>()
 
   useEffect(() => {
-    connect()
-
-    return disconnect
+    axios.get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then( res => setUsers(res.data))
   })
+
   
   return (
-    <div>
-      App
-    </div>
+    <ul>
+      {users?.map( u => <li key={u.id}>Id: {u.id}, Name: {u.name}</li> )}
+    </ul>
   );
 };
 
