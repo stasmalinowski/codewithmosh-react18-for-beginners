66abb6ead7ce3bf3b7538cd8b5b5678c38f79b5b
stasmalinowski <stasmalinowski@proton.me>
2024-07-16 10:46:33 +0200

    Play with handling fetch errors

--- follow_along/src/App.tsx
+++ follow_along/src/App.tsx
@@ -10,17 +10,23 @@ interface User{
 const App = () => {
 
   const [users, setUsers] = useState<User[]>()
+  const [error, setError] = useState<string>("")
 
   useEffect(() => {
-    axios.get<User[]>("https://jsonplaceholder.typicode.com/users")
+    axios.get<User[]>("https://jsonplxxxaceholder.typicode.com/users")
       .then( res => setUsers(res.data))
+      .catch( err => setError(err.message) )
   }, [])
   
   return (
+  <>
+    <p className="text-danger">{error}</p>
     <ul>
       {users?.map( u => <li key={u.id}>Id: {u.id}, Name: {u.name}</li> )}
     </ul>
-  );
+  </>
+    );
+  
 };
 
 export default App;
