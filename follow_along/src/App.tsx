087c452273f34cdbdfb9b4f6df1ee14268f81efc
stasmalinowski <stasmalinowski@proton.me>
2024-07-16 10:25:12 +0200

    Play around with useEffect cleanup functions

--- follow_along/src/App.tsx
+++ follow_along/src/App.tsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ProductList } from "./components/ProductList";
 
 const App = () => {
-  const [ category, setCategory ] = useState("")
 
+  const connect = () => console.log("Connecting...")
+  const disconnect = () => console.log("Disconnecting...")
+
+  useEffect(() => {
+    connect()
+
+    return disconnect
+  })
+  
   return (
     <div>
-      <select name="" id="" className="form-select" onChange={event => setCategory(event.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
+      App
     </div>
   );
 };
