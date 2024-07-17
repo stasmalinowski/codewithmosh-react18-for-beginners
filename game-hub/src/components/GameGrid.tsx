1b3018366335c890cd4d2e165900b99bb46401b6
stasmalinowski <stasmalinowski@proton.me>
2024-07-17 12:16:13 +0200

    Create useGames hook for fetching games

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -1,20 +1,9 @@
-import { useState, useEffect } from "react";
-import GameService, { Game } from "../services/game-service";
 import { Text } from "@chakra-ui/react";
+import { useGames } from "../hooks/useGames";
 
 
 export const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState<string>("");
-
-  useEffect(() => {
-    GameService.getAll()
-      .then((games) => {
-        setGames(games);
-        setError("")
-      })
-      .catch((err) => setError(err.message));
-  });
+  const {games, error} = useGames()
 
   return (
     <>
