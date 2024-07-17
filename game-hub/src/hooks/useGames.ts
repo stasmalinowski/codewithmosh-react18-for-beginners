be4210af99908cf778b0126850d4e12022c84fab
stasmalinowski <stasmalinowski@proton.me>
2024-07-17 12:22:32 +0200

    Create useGames hook for fetching games from the API

---
+++ game-hub/src/hooks/useGames.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import GameService, { Game } from "../services/game-service";
+
+export const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    GameService.getAll()
+      .then((games) => {
+        setGames(games);
+        setError("")
+      })
+      .catch((err) => setError(err.message));
+  });
+
+  return {games, setGames, error, setError}
+}
