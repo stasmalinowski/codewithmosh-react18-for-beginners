f950d2021b0bc83fa35de2f157c609b64fa27fcb
stasmalinowski <stasmalinowski@proton.me>
2024-07-18 13:21:01 +0200

    Add the ability to pass query parameters in useEntityFetch and useGames

--- game-hub/src/hooks/useEntityFetch.ts
+++ game-hub/src/hooks/useEntityFetch.ts
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
 import { EntityFetchService } from "../services/http-service";
 
-export function useEntityFetch<Entity>(service: EntityFetchService<Entity>): {entities: Entity[], error: string, isLoading: boolean} {
+export function useEntityFetch<Entity>(
+  service: EntityFetchService<Entity>,
+  params?: object
+): { entities: Entity[]; error: string; isLoading: boolean } {
   const [entities, setEntities] = useState<Entity[]>([]);
   const [error, setError] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     setIsLoading(true);
-    service.getAll()
+    service
+      .getAll(params ? params : {})
       .then((e) => {
         setEntities(e);
         setError("");
@@ -16,9 +20,9 @@ export function useEntityFetch<Entity>(service: EntityFetchService<Entity>): {en
       })
       .catch((err) => {
         setError(err.message);
-        setIsLoading(false)
+        setIsLoading(false);
       });
   }, []);
 
   return { entities, error, isLoading };
-};
+}
--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,7 +1,14 @@
 import GameService, { Game } from "../services/game-service";
+import { Genre } from "../services/genre-service";
 import { useEntityFetch } from "./useEntityFetch";
 
-export const useGames = () => {
-  const {entities, error, isLoading} = useEntityFetch<Game>(GameService)
+export const useGames = (genreFilter?: Genre[]) => {
+  const params = genreFilter
+    ? {
+        genres: genreFilter.map((g) => g.id).join(","),
+      }
+    : {};
+
+  const { entities, error, isLoading } = useEntityFetch<Game>(GameService, params);
   return { games: entities, error, isLoading };
 };
